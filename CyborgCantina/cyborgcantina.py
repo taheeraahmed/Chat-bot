0ed4207160bd925f743c28ef43826801789ec11a
Taheera Ahmed <taheera@hotmail.com>
2021-06-14 16:09:05 +0200

    Adding files and imports to CyborgCantina

---
+++ CyborgCantina/cyborgcantina.py
@@ -0,0 +1,27 @@
+from collections import Counter
+from CyborgCantina.responses import responses, blank_spot
+from CyborgCantina.user_functions import preprocess, compare_overlap, pos_tag, extract_nouns, compute_similarity
+import spacy
+word2vec = spacy.load('en')
+
+exit_commands = ("quit", "goodbye", "exit", "no")
+
+class ChatBot:
+  
+  #define .make_exit() below:
+      
+  #define .chat() below:
+  
+  #define .find_intent_match() below:
+ 
+  #define .find_entities() below:
+ 
+  #define .respond() below:
+  
+
+#initialize ChatBot instance below:
+
+#call .chat() method below:
+
+
+
