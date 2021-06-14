d6f665bda66e60944260fd479d56a0cde87c6c63
Taheera Ahmed <taheera@hotmail.com>
2021-06-15 01:38:43 +0200

    Done with the CyborgCantina

--- CyborgCantina/cyborgcantina.py
+++ CyborgCantina/cyborgcantina.py
@@ -1,6 +1,6 @@
 from collections import Counter
-from CyborgCantina.responses import responses, blank_spot
-from CyborgCantina.user_functions import preprocess, compare_overlap, pos_tag, extract_nouns, compute_similarity
+from responses import responses, blank_spot
+from user_functions import preprocess, compare_overlap, pos_tag, extract_nouns, compute_similarity
 import spacy
 word2vec = spacy.load('en')
 
@@ -9,19 +9,59 @@ exit_commands = ("quit", "goodbye", "exit", "no")
 class ChatBot:
   
   #define .make_exit() below:
+  def make_exit(self, user_message):
+    for exit_command in exit_commands:
+      if exit_command in user_message:
+        print('Okay, bye!')
+        return True
+      else:
+        continue
       
   #define .chat() below:
-  
+  def chat(self):
+    user_message = input('Hi, what can I do for you, human?\n')
+    while not self.make_exit(user_message):
+      user_message = self.respond(user_message)
+      
   #define .find_intent_match() below:
- 
+  def find_intent_match(self, responses, user_message):
+    bow_user_message = Counter(preprocess(user_message))
+
+    bow_response = [Counter(preprocess(response)) for response in responses]
+
+    similarity_list = [compare_overlap(doc, bow_user_message) for doc in bow_response]
+    response_index = similarity_list.index(max(similarity_list))
+
+    return responses[response_index]
+    
   #define .find_entities() below:
- 
+  def find_entities(self,user_message):
+    tagged_user_message = pos_tag(preprocess(user_message))
+    message_nouns = extract_nouns(tagged_user_message)
+
+    tokens = word2vec(" ".join(message_nouns))
+    category = word2vec(blank_spot)
+    word2vec_result = compute_similarity(tokens, category)  
+
+    word2vec_result.sort(key=lambda x: x[2])
+    if len(word2vec_result) < 1:
+      return blank_spot
+    else:
+      return word2vec_result[-1][0]
+
   #define .respond() below:
-  
+  def respond(self, user_message):
+    best_response = self.find_intent_match(responses, user_message)
+    entity = self.find_entities(user_message)
+    print(best_response.format(entity))
+    input_message = input("Do you have any other questions? \n")           
+    return input_message
 
 #initialize ChatBot instance below:
+chat_bot = ChatBot()
 
 #call .chat() method below:
+chat_bot.chat()
 
 
 
--- CyborgCantina/user_functions.py
+++ CyborgCantina/user_functions.py
@@ -7,6 +7,9 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 stop_words = set(stopwords.words("english"))
 
+# Preprocesses the input sentence given by the user
+# Removes uppercases, whitespaces, tokenizes 
+# Return an array of the strings given by the user
 def preprocess(input_sentence):
     input_sentence = input_sentence.lower()
     input_sentence = re.sub(r'[^\w\s]','',input_sentence)
@@ -14,13 +17,19 @@ def preprocess(input_sentence):
     input_sentence = [i for i in tokens if not i in stop_words]
     return(input_sentence)
 
+# Compares two different strings and sees if 
+# they have anything in common aka. if there are any 
+# similar words
+# Returns this number
 def compare_overlap(user_message, possible_response):
     similar_words = 0
     for token in user_message:
         if token in possible_response:
               similar_words += 1
     return similar_words
-  
+
+# Uses pos-tagged user message and extracts nouns 
+# from the input sentence and is used for entitiy extraction 
 def extract_nouns(tagged_message):
     message_nouns = list()
     for token in tagged_message:
@@ -28,6 +37,8 @@ def extract_nouns(tagged_message):
             message_nouns.append(token[0])
     return message_nouns
 
+# Computes similarity given a token and a category 
+# Returns a 2D array of the token, the category and the similarity
 def compute_similarity(tokens, category):
     output_list = list()
     for token in tokens:
